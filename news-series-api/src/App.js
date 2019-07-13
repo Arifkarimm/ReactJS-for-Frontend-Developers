2e0e34ae2d5cde7f44d7b176ecafb2e9f2c0d83b
Fazlul <fkarif.cse@gmail.com>
2019-07-13 03:13:01 +0200

    Complex Javascript in JSX

--- news-series-api/src/App.js
+++ news-series-api/src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+	{
+		title: "React",
+		url: "https://reactjs.org/",
+		author: "Fazlul",
+		num_commemts: 4,
+		points: 3,
+		objectID: 0,
+	},
+
+	{
+		title: "Redux",
+		url: "https://redux.js.org/",
+		author: "Karim",
+		num_commemts: 5,
+		points: 2,
+		objectID: 1,
+	},
+];
 
 class App extends Component {
 
   render(){
-    const message = "Welcome to Reactjs For Frontend Developers";
     return(
       <div className="App">
-       <h2>{ message }</h2>
+       {list.map(function(item){ 
+	       	return(
+	       		<div key={item.objectID} className="fetch-item-data">
+	       			<span><a href={item.title}>{item.title}</a></span>
+	       			<span>{item.author}</span>
+	       			<span>{item.num_commemts}</span>
+	       			<span>{item.points}</span>
+	       		</div>
+	       	);
+       })}
       </div>
     )
   }
