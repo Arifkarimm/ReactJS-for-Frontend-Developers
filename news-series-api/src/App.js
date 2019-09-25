659ce2ad449d27e3a54bb535ad7f5f5cf5330eac
Fazlul <fkarif.cse@gmail.com>
2019-09-25 18:53:35 +0200

    JS Includes Function

--- news-series-api/src/App.js
+++ news-series-api/src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+	{
+		title: "React",
+		url: "https://reactjs.org/",
+		author: "Fazlul",
+		num_commemts: 4,
+		points: 3,
+		objectID: 0,
+	},
 
+	{
+		title: "Redux",
+		url: "https://redux.js.org/",
+		author: "Karim",
+		num_commemts: 5,
+		points: 2,
+		objectID: 1,
+	},
+];
 
 class App extends Component {
 
-	constructor(){
-		super();
+	constructor(props){
+		super(props);
 
-		this.onClickMe = () => {
-			console.log(this);
-		}		
+		this.state = {
+			list
+		}
+
+		this.onDismiss = this.onDismiss.bind(this);
 	}
 
-	
+	onDismiss(id){
+		// function isNotid(item){
+		// 	return item.objectID !== id;
+		// }
 
-	
+		const isNotid = item => item.objectID !== id;
+		
+
+		const updateList = this.state.list.filter(isNotid);
+
+		// const updateList = this.state.list.filter(item => item.objectID !== id);
+
+		this.setState({ list: updateList })
+	}
 
   render(){
     return(
-      <button type="button" onClick={this.onClickMe}>Click me</button>
+      <div className="App">
+       {this.state.list.map(item =>(
+	       		<div key={item.objectID} className="fetch-item-data">
+	       			<span><a href={item.title}>{item.title}</a></span>
+	       			<span>{item.author}</span>
+	       			<span>{item.num_commemts}</span>
+	       			<span>{item.points}</span>
+	       			<span>
+	       				<button type="button" onClick={() => this.onDismiss(item.objectID)}>Dismiss</button>
+	       			</span>
+	       		</div>
+	       	)
+       )}
+      </div>
     )
   }
 }
